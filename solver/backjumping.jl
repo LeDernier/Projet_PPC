b849c94199f5fac71cd2860c8593f7bb91b590f3
Louise Eymery <louise.eymery@eleves.enpc.fr>
2022-12-26 15:32:48 +0100

    update backjumping algo

--- solver/backjumping.jl
+++ solver/backjumping.jl
@@ -1,62 +1,104 @@
-function consistant(instance::Problem)
-    return true
-end
 
+module BackjumpingSolver
 
-function selectValue(D::Vector, i::Tnt, latest::Int)
-    while length(D) > 0
-        a = pop!(D)
-        consist = true
-        k = 1
-        while k < i && consist
-            if k > latest
-                latest = k
-            end
-            if !consistant() # to change
-                consist = false
-            else
-                k += 1
+    using ..Instance: Problem, getVariable        # .. because the include(..model/instance.jl) should be done in the file that includes this file
+
+    export backjumping
+
+    
+    function consistant(instance::Problem, var_names::Array, k::Int, i::Int, a::Real)
+        # check that all constraints are ok with the affectation
+        # do we need to check a1, ... aK ? or only current a ?
+        x_name = var_names[i]
+        for c in instance.constraints
+            var1 = c.varsIDs[1] # use getVariable here !!!!
+            var2 = c.varsIDs[2] # use getVariable here !!!!
+            if var1.name in var_names[1:k]
+                if var2.name in var_names[1:k]
+                    if !((var1.value, var2.value) in c.feasible_points)
+                        return false
+                    end
+                elseif var2.name == x_name && !((var1.value, a) in c.feasible_points)
+                    return false
+                elseif !(var1.value in [p[1] for p in c.feasible_points])
+                    return false
+                end
+            elseif var2.name in var_names[1:k]
+                if var1.name == x_name && !((a, var2.value) in c.feasible_points)
+                    return false
+                elseif !(var2.value in [p[2] for p in c.feasible_points])
+                    return false
+                end
             end
         end
-        if consist
-            return a, latest
-        end
+        return true
     end
-    return -1, latest
-end
 
 
-function backjumping(instance::Problem)
-    n = length(instance.variables)
-    values = Array{Real, 1}(undef, n)
-    domains = Array{Vector, 1}(undef, n)
-    var_names = collect(keys(instance.variables))
-    i = 1
-    latest = 0
-    var = instance.variables[var_names[i]]
-    domain = copy(var.domain)
-    while i >= 0 && i <= n
-        val = selectValue(domain, i)
-        domains[i] = domain
-        values[i] = val
-        if val == -1
-            i = latest
-            if isdefined(domains, i)
-                domain = domains[i]
+    function selectValue(instance::Problem, var_names::Array, D::Vector, i::Tnt, latest::Int, var::Variable)
+        while length(D) > 0
+            a = pop!(D)
+            consist = true
+            k = 1
+            while k < i && consist
+                if k > latest
+                    latest = k
+                end
+                if !consistant(instance, var_names, k, i, a)
+                    consist = false
+                else
+                    k += 1
+                end
+            end
+            if consist
+                return a, latest
+            end
+        end
+        return undef, latest
+    end
+    
+    
+    function backjumping(instance::Problem)
+        n = length(instance.variables)
+        latests = Array{Int, 1}(undef, n)
+        domains = Array{Vector, 1}(undef, n)
+        var_names = collect(keys(instance.variables)) 
+        # TODO: sort var_names so that we go through the list in a better order
+        # for instance, sort in increasing domain size
+    
+        # TODO: avoid copies of domains, use index_domain instead
+    
+        i = 1
+        latest = 0
+        var = instance.variables[var_names[i]]
+        domain = copy(var.domain)
+        while i > 0 && i <= n        
+            var = instance.variables[var_names[i]]
+            val, latest = selectValue(instance, var_names, domain, i, latest)
+            domains[i] = domain
+            latests[i] = latest
+            if val == undef
+                i = latest
+                if isdefined(domains, i)
+                    domain = domains[i]
+                else
+                    domain = copy(var.domain)
+                end
             else
+                var.value = val
+                i += 1
+                latest = 0
                 var = instance.variables[var_names[i]]
                 domain = copy(var.domain)
             end
+        end
+    
+        if i == 0
+            return false # inconsistant
         else
-            i += 1
-            latest = 0
-            var = instance.variables[var_names[i]]
-            domain = copy(var.domain)
+            return true  # we found a solution
         end
     end
-    if i == 0
-        return -1  # inconsistant
-    else
-        return values  # we found a solution
-    end
 end
+
+
